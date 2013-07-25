c565bd3a9d4cf43a871db6265e8502797f31c578
Rohith sudhakara <rsud2253@gmail.com>
2013-07-25 10:33:05 +0000

    adding BITSTARTER

---
+++ bitstarter/web.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+var express = require('express');
+var app = express();
+var fs = require('fs');
+app.use(express.logger());
+
+var content = fs.readFileSync('./index.html', 'utf8');
+
+
+app.get('/', function(request, response) {
+  response.send(content);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
